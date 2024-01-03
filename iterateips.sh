25b49e07f56cf181598021b64e1076850ac0a651
Jonathan Wallace <jwallaceni@gmail.com>
2024-01-03 21:06:00 +0000

    Update iterateips.sh

--- iterateips.sh
+++ iterateips.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo ""
-echo "Resolving IP addresses..."
+echo "Resolving IP addresses from $1..."
 echo ""
 while read p; do
   (getent hosts $p | grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])');
