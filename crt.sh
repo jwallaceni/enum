fd5251866a121d3024a05f7da2f338cee17a006a
Jonathan Wallace <jwallaceni@gmail.com>
2024-01-04 17:41:54 +0000

    Update crt.sh

--- crt.sh
+++ crt.sh
@@ -1,7 +1,8 @@
+  GNU nano 7.2                                                                crt.sh                                                                         
 #!/bin/bash
 echo ""
 echo "Gathering domain info from crt.sh for $1"
 echo ""
-curl -s 'https://crt.sh/?q='"$1"'&output=json' | jq -r '.[] | .name_value' | sort -u
+curl -s 'https://crt.sh/?q='"$1"'&output=json' | jq -r '.[] | .name_value' | sort -u | sed 's/*.//g'
 echo ""
 echo "Done"
