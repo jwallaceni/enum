5f4cb1ac60b06642764039fa08ff78791981dd9e
Jonathan Wallace <jwallaceni@gmail.com>
2024-01-04 17:44:16 +0000

    Update crt.sh

--- crt.sh
+++ crt.sh
@@ -2,6 +2,6 @@
 echo ""
 echo "Gathering domain info from crt.sh for $1"
 echo ""
-curl -s 'https://crt.sh/?q='"$1"'&output=json' | jq -r '.[] | .name_value' | sort -u | sed 's/*.//g'
+curl -s 'https://crt.sh/?q='"$1"'&output=json' | jq -r '.[] | .name_value' | sed 's/*.//g' | sort -u
 echo ""
 echo "Done"
